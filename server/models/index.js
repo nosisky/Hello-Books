af625bda53de8305ad99b8f1ab18640c6e25cb48
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2017-08-02 11:47:17 +0100

    modify index.js file to use ES6 syntax

--- server/models/index.js
+++ server/models/index.js
@@ -1,30 +1,30 @@
-'use strict';
+import fs from 'fs';
+import path from 'path';
 
-const fs        = require('fs');
-const path      = require('path');
-const Sequelize = require('sequelize');
-const basename  = path.basename(module.filename);
-const env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/..\config\config.json')[env];
-const db        = {};
+import Sequelize from 'sequelize';
+
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '/..\config\config.json'))[env];
+const db = {};
 
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable]);
+  const sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  let sequelize = new Sequelize(config.database, config.username, config.password, config);
+  const sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(function(file) {
+  .filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(function(file) {
-    let model = sequelize['import'](path.join(__dirname, file));
+  .forEach((file) => {
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
