f400dace8ff82caa91889ddac7a0b6eb781f18fd
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2017-08-02 11:46:49 +0100

    modify the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+import express from 'express';
+
+import logger from 'morgan';
+
+import bodyParser from 'body-parser';
+
+import winston from 'winston';
 
 // Set up the express app
 const app = express();
@@ -10,11 +14,14 @@ app.use(logger('dev'));
 
 // Parse incoming requests data 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+const port = 8000;
+
+app.listen(port, () => {
+  winston.info(`Connected to port: ${port}`);
+});
 
 module.exports = app;
