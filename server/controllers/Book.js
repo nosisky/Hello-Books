93264f36319fa079a710526ae2aab67b8f822daa
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2017-08-03 16:31:13 +0100

    create function to  modify book information

--- server/controllers/Book.js
+++ server/controllers/Book.js
@@ -5,12 +5,10 @@ export default {
   create(req, res) {
     return Book
       .create(req.userInput)
-      .then(() => {
-        return res.status(201).send({
-          success: true,
-          message: 'Book uploaded successfully',
-        });
-      })
+      .then(() => res.status(201).send({
+        success: true,
+        message: 'Book uploaded successfully',
+      }))
       .catch(error => res.status(400).send(error));
   },
   getBooks(req, res) {
@@ -18,5 +16,34 @@ export default {
       .findAll({})
       .then(books => res.status(201).send(books))
       .catch(error => res.status(404).send(error));
+  },
+  modify(req, res) {
+    return Book
+      .findById(req.params.id, {
+        include: [{
+          model: Book,
+          as: 'books',
+        }],
+      })
+      .then(books => {
+      if (!books) {
+        return res.status(404).send({
+          message: 'Book With Id Not Found',
+        });
+      }
+      return Book
+      .update({
+          title: req.body.title || book.title,
+          isbn: req.body.isbn || book.isbn,
+          prodYear: req.body.prodYear || book.prodYear,
+          cover: req.body.title || book.title,
+          author: req.body.author || book.author,
+          description: req.body.description || book.description
+        })
+        .then(() => res.status(200).send(book))
+        .catch((error) => res.status(400).send(error));
+    })
+    .catch((error) => res.status(400).send(error));
+},
   }
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -2,13 +2,14 @@ import express from 'express';
 
 import BookController from '../controllers/Book';
 import Validation from '../middleware/Validation';
+//import Authorization from '../middleware/Authorization';
 
 const app = express.Router();
 
 app.route('/')
   .post(Validation.checkUserInput, BookController.create)
   .get(BookController.getBooks);
-// app.route('/:id')
-//   .put(Validation.validateLogin, BookController.login);
+app.route('/:id')
+  .put(BookController.modify);
 
 export default app;
