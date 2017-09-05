86ecc06a06c3d584bc3c52f6e5b09f1544d83d90
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2017-09-05 13:51:44 +0100

    fix user authentication issues

--- client/components/includes/header-side-bar.js
+++ client/components/includes/header-side-bar.js
@@ -3,116 +3,49 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 export default class HeaderSideBar extends Component {
-    render(){
-        return(
-            <div>
-            <ul id="slide-out" className="side-nav fixed z-depth-2">
-    <li className="center no-padding">
-      <div className="header-side white-text" style={{height: 180}}>
-        <div className="row">
-          <img style={{marginTop: 5}} width="100" height="100" src="http://www.clker.com/cliparts/f/b/6/4/1194984600870878528adult_astrid_graeber_01.svg.hi.png" className="circle responsive-img" />
+  render() {
+    return (<div className="header-side" id="container">
+      <ul id='dropdown1' className='dropdown-content'>
+        <li><a href="#!"> {this.props.username}</a></li>
+        <li className="divider"></li>
+        <li><a onClick={this.props.onClick} href="#"><i className="material-icons">exit_to_app</i> Logout</a></li>
+        <li><a href="#!"><i className="material-icons">account_circle</i> Profile</a></li>
+      </ul>
 
-          <p style={{marginTop: -13}}>
-            {this.props.fullname}
-          </p>
+      <div id="menu">
+        <div style={{ float: 'right', color: '#fff', padding: 5 }}>
+          <a style={{ float: 'right', marginLeft: 5, backgroundColor: '#82ccc5' }}
+            className='dropdown-button btn' href='#' data-activates='dropdown1'>Account</a>
         </div>
-      </div>
-    </li>
-
-    <li id="dash_dashboard"><a className="waves-effect" href="#!"><b>Dashboard</b></a></li>
-
-    <ul className="collapsible" data-collapsible="accordion">
-      <li id="dash_users">
-        <div id="dash_users_header" className="collapsible-header waves-effect"><b>Books</b></div>
-        <div id="dash_users_body" className="collapsible-body">
-          <ul>
-            <li id="users_seller">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Rented Books</a>
-            </li>
-
-            <li id="users_customer">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Rent a Book</a>
-            </li>
-            <li id="users_customer">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Return Book</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-
-      <li id="dash_products">
-        <div id="dash_products_header" className="collapsible-header waves-effect"><b>Products</b></div>
-        <div id="dash_products_body" className="collapsible-body">
-          <ul>
-            <li id="products_product">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Products</a>
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Orders</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-
-      <li id="dash_categories">
-        <div id="dash_categories_header" className="collapsible-header waves-effect"><b>History</b></div>
-        <div id="dash_categories_body" className="collapsible-body">
-          <ul>
-            <li id="categories_category">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Category</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-
-      <li id="dash_brands">
-        <div id="dash_brands_header" className="collapsible-header waves-effect"><b>Brands</b></div>
-        <div id="dash_brands_body" className="collapsible-body">
-          <ul>
-            <li id="brands_brand">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Brand</a>
-            </li>
-
-            <li id="brands_sub_brand">
-              <a className="waves-effect" style={{textDecoration: "none"}} href="#!">Sub Brand</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-    </ul>
-  </ul>
-
-  <header>
-    <ul className="dropdown-content" id="user_dropdown">
-      <li><a className="indigo-text" href="#!">Profile</a></li>
-      <li><a className="indigo-text" onClick={this.props.onClick} href="">Logout</a></li>
-    </ul>
-
-    <nav className="header-side" role="navigation">
-      <div className="nav-wrapper">
-        <a data-activates="slide-out" className="button-collapse show-on- text-large" href="#!">HelloBooks</a>
-
-        <ul className="right hide-on-med-and-down">
-          <li>
-            <a className='right dropdown-button' href='' data-activates='user_dropdown'><i className=' material-icons'>account_circle</i></a>
-          </li>
-        </ul>
-
-        <a href="#" data-activates="slide-out" className="button-collapse"><i className="mdi-navigation-menu"></i></a>
-      </div>
-    </nav>
-
-    <nav>
-      <div className="nav-wrapper header-side darken-2">
-        <a style={{marginLeft: 20}} className="breadcrumb" href="#!">Admin</a>
-        <a className="breadcrumb" href="#!">Index</a>
-
-        <div style={{marginRight: 20}} id="timestamp" className="right"></div>
-      </div>
-    </nav>
-  </header>
-
+        <div className="col s3">
+          <ul id="slide-out" className="side-nav fixed show-on-large-only">
+            <div style={{ textAlign: 'center', color: '#000', backgroundColor: '#25758c', marginTop: -16 }}>
+              <div className="row style={{backgroundColor: '#25758c'}}">
+                <span className="card-title"><h4><i className="material-icons">library_books</i> HelloBooks</h4></span>
+                <li className="divider"></li>
+                <p></p>
+                <img style={{ borderRadius: 50, border: '2px solid black' }}
+                  width="100px"
+                  height="100px"
+                  src="https://images.vexels.com/media/users/3/130527/isolated/preview/845f79841ea58765d623a68bf434d5ed-girl-cartoon-head-character-by-vexels.png"
+                  alt="HelloBooks" /><br />
+                <i className="material-icons">account_circle</i> <b>{this.props.username}</b>
+              </div><br />
             </div>
-        )
-    }
+            <li className="divider"></li>
+            <li id="menu-list"><a href="#!">Rent History <i className="material-icons">chevron_right</i></a></li>
+            <li id="menu-list"><a href="#!">Rent History <i className="material-icons">chevron_right</i></a></li>
+            <li id="menu-list"><a href="#!">Rent a Book <i className="material-icons">chevron_right</i></a></li>
+            <li id="menu-list"><a href="#!">Rented Books <i className="material-icons">chevron_right</i></a></li>
+            <li id="menu-list"><a href="#!">Rent History <i className="material-icons">chevron_right</i></a></li>
+          </ul>
+        </div></div>
+      <div id="content">
+        <a href="#" data-activates="slide-out" className="button-collapse hide-on-large-only"><i style={{ color: '#fff', fontSize: 40 }} className="material-icons">menu</i></a>
+
+      </div>
+    </div>)
+  }
 }
 
 HeaderSideBar.PropTypes = {
--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
-  $('.button-collapse').toggle(() => {
-    $(this).toggleClass('.collapsible');
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
   });
-
-  $('select').material_select();
 });
