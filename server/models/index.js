db3c75c5b5d919807e03d9b0566fa27b12208fc0
nosisky <nosisky@gmail.com>
2017-10-24 18:00:10 +0100

    Fix failing travis build

--- server/models/index.js
+++ server/models/index.js
@@ -10,7 +10,7 @@ const config = dbConfig[env];
 const db = {};
 dotenv.config();
 let sequelize;
-if (process.env.DATABASE_URL) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
