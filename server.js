7a78381a44ba676929fb5c427620eac8d2c50a52
nosisky <nosisky@gmail.com>
2017-10-30 02:12:07 +0100

    feature(add-api-documentation): Implement API documentation functionality

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
+import swagger from 'swagger-jsdoc';
 import webpack from 'webpack';
 import winston from 'winston';
 import webpackMiddleware from 'webpack-dev-middleware';
@@ -16,8 +17,34 @@ import SearchRouter from './server/routes/search';
 const app = express();
 
 dotenv.load();
+
+const swaggerJSDoc = swagger;
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'HelloBooks API',
+    version: '1.0.0',
+    description: 'An application that helps manage a library and its processes like stocking, tracking and renting of books.',
+  },
+  host: 'andela-hellobooks.herokuapp.com',
+
+  basePath: '/api/v1',
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./server/routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
 app.use(logger('dev'));
 app.use(express.static('./client/public/')); // configure static files folder
+app.use('/api/docs/', express.static(path.join(__dirname, 'server/api-docs/')));
 app.use(webpackMiddleware(webpack(webpackConfig)));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +58,11 @@ app.use('/api/v1/books', BookRouter);
 
 app.use('/api/v1/search', SearchRouter);
 
+// serve swagger
+app.get('/api/docs/hellobooks.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/index.html'));
--- server/routes/books.js
+++ server/routes/books.js
@@ -5,7 +5,65 @@ import Validation from '../middleware/Validation';
 
 const app = express.Router();
 
-// Add a new book
+/**
+ * @swagger
+ * definitions:
+ *   Book:
+ *     properties:
+ *       isbn:
+ *         type: integer
+ *       title:
+ *         type: string
+ *       author:
+ *         type: string
+ *       description:
+ *         type: string
+ *       quantity:
+ *         type: integer
+ *       cover:
+ *         type: string
+ *       prodYear: 
+ *         type: integer
+ */
+
+ /**
+ * @swagger
+ * definitions:
+ *   Category:
+ *     properties:
+ *       name:
+ *         type: string
+ *       description:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     tags:
+ *       - Book Operations
+ *     description: Adds a new book to the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: book
+ *         description: Book object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Book'
+ *       - name: x-access-token
+ *         in: header
+ *         description: an authentication header
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ *       400:
+ *         description: Bad Username, Password or Email
+ */
 app.route('/')
   .post(Authorization.isLoggedIn,
     Authorization.isAdmin,
@@ -13,14 +71,75 @@ app.route('/')
     BookController.create)
   .get(Authorization.isLoggedIn, BookController.getBooks);
 
-// Modify Book Information
+/**
+ * @swagger
+ * /books/{bookId}:
+ *   put:
+ *     tags:
+ *       - Book Operations
+ *     description: Modify an already added Book's information
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: bookId
+ *         description: ID of the Book
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: book
+ *         description: Book object with updated information
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Book'
+ *       - name: x-access-token
+ *         in: header
+ *         description: an authentication header
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ *         schema:
+ *           $ref: '#/definitions/Book'
+ *       400:
+ *         description: All fields are required
+ *       404:
+ *         description: Book not found
+ */
 app.route('/:bookId')
   .put(Authorization.isLoggedIn,
     Authorization.isAdmin,
     Authorization.validBook,
     BookController.modifyBook);
 
-// Adds a new category to the database
+/**
+ * @swagger
+ * /books/cat:
+ *   post:
+ *     tags:
+ *       - Book Operations
+ *     description: Adds a new category to the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: category
+ *         description: Category object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Category'
+ *       - name: x-access-token
+ *         in: header
+ *         description: an authentication header
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ *       400:
+ *         description: Bad Username, Password or Email
+ */
 app.route('/cat')
   .post(Authorization.isLoggedIn,
     Authorization.isAdmin,
@@ -32,20 +151,88 @@ app.route('/:bookId')
     Authorization.validBook,
     BookController.getOneBook);
 
-// Delete a book
+/**
+ * @swagger
+ * /books/delete/{bookId}:
+ *   delete:
+ *     tags:
+ *       - Book Operations
+ *     description: Delete a specified Book
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: bookId
+ *         description: ID of the Book
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: x-access-token
+ *         in: header
+ *         description: an authentication header
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Book successfully deleted
+ *       400:
+ *         description: Enter valid inputs!
+ *       404:
+ *         description: Book not found
+ */
 app.route('/delete/:bookId')
   .delete(Authorization.isLoggedIn,
     Authorization.isAdmin,
     Authorization.validBook,
     BookController.deleteBook);
 
-// Get rented books by a user
+/**
+ * @swagger
+ * /books/logs/:userId:
+ *   get:
+ *     tags:
+ *       - Book Operations
+ *     description: Returns all book browsing history of a user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: x-access-token
+ *         in: header
+ *         description: an authentication header
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: An array of Categories
+ *         schema:
+ *           $ref: '#/definitions/Book'
+ */
 app.route('/logs/:userId')
   .get(Authorization.isLoggedIn,
     Authorization.validUser,
     BookController.rentedBookByUser);
 
-// Get All category
+
+/**
+ * @swagger
+ * /books/category:
+ *   get:
+ *     tags:
+ *       - Book Operations
+ *     description: Returns all Category in the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: x-access-token
+ *         in: header
+ *         description: an authentication header
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: An array of Categories
+ *         schema:
+ *           $ref: '#/definitions/Book'
+ */
 app.route('/category')
   .get(Authorization.isLoggedIn,
     Authorization.validUser,
