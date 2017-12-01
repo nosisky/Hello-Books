d7d5feae5da2f2d13460309fd03a7e76b0a60e61
nosisky <nosisky@gmail.com>
2017-12-01 09:02:57 +0100

    chore(fix-missing-JSDOC): Add missing JSDOC

--- client/utils/Notifications.js
+++ client/utils/Notifications.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const API_URL = '/api/v1/notification';
 
 
+/**
+ * 
+ * @returns {Object} - Object containing notifications
+ */
 export default function Notifications() {
   return axios.get(API_URL)
     .then(response => response.data)
